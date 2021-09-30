f18ef2bfd8a2bb8979c772691408da47b28aed44
Shoon San <78584709+shoonlaeyee@users.noreply.github.com>
2021-10-01 00:02:44 +0630

    Update SpeakHello.js

--- module4_sol/SpeakHello.js
+++ module4_sol/SpeakHello.js
@@ -1,13 +1,3 @@
-(function(window) {
-  var speakWord = "Hello";
-  var helloSpeaker = function (name) {
-    console.log(speakWord + " " + name);
-  }
-
-  window.helloSpeaker = helloSpeaker;
-
-})(window);
-
 var helloSpeaker = {
     speakWord : "Hello ",
     speak : function(name) {
